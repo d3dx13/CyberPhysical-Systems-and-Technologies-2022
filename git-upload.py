2a182865b9dec2670a659cbda6f44ca1f3dd786d
d3dx13 <zhidkov2846@gmail.com>
2022-09-15 12:37:04 +0300

    lab1 - Тест руского автокоммита

--- git-upload.py
+++ git-upload.py
@@ -8,8 +8,12 @@ if len(commit_name) == 0:
 path_dirs = [name for name in os.listdir(os.getcwd()) if
              os.path.isdir(os.path.join(os.getcwd(), name)) and not name.startswith(".")]
 
+os.system(f'git config credential.helper store')
+
 for path_dir in path_dirs:
     os.system(f'cd {path_dir}')
+
+    os.system(f'git pull')
 
     os.system(f'git reset')
     os.system(f'git add .')
