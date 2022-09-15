2ef2b81a58375faa20d9fb57e875dea334fd07ba
d3dx13 <zhidkov2846@gmail.com>
2022-09-15 12:39:11 +0300

    Тест руского автокоммита

--- git-upload.py
+++ git-upload.py
@@ -9,15 +9,19 @@ path_dirs = [name for name in os.listdir(os.getcwd()) if
              os.path.isdir(os.path.join(os.getcwd(), name)) and not name.startswith(".")]
 
 os.system(f'git config credential.helper store')
+os.system(f'git pull')
 
 for path_dir in path_dirs:
     os.system(f'cd {path_dir}')
 
-    os.system(f'git pull')
-
     os.system(f'git reset')
-    os.system(f'git add .')
+    os.system(f'git add {path_dir}')
     os.system(f'git commit -m \"{path_dir + " - " + commit_name}\"')
     os.system(f'git push')
 
     os.system(f'cd ..')
+
+os.system(f'git reset')
+os.system(f'git add .')
+os.system(f'git commit -m \"{commit_name}\"')
+os.system(f'git push')
