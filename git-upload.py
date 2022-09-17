44322dbd6454833850b864e3773c493646cf663f
d3dx13 <zhidkov2846@gmail.com>
2022-09-17 17:56:32 +0300

    pdf ещё одна попытка

--- git-upload.py
+++ git-upload.py
@@ -19,8 +19,8 @@ for path_dir in path_dirs:
                          ".ipynb")]
     for jupyter_file in jupyter_files:
         os.system(f'jupyter nbconvert {jupyter_file} --to markdown --output {jupyter_file[:-6]}')
-        os.system(f'jupyter nbconvert {jupyter_file} --to pdf --output \"{f"{jupyter_file[:-6]} - Отчёт Жидков А.А. R4136с "}\"')
-        # os.system(f'pandoc {jupyter_file} --pdf-engine=pdflatex --output {jupyter_file[:-6]}.pdf')
+        # os.system(f'jupyter nbconvert {jupyter_file} --to pdf --output \"{f"{jupyter_file[:-6]} - Отчёт Жидков А.А. R4136с "}\"')
+        os.system(f'pandoc {jupyter_file} --pdf-engine=pdflatex --output {jupyter_file[:-6]}.pdf')
         # pandoc MANUAL.txt --pdf-engine=xelatex -o example13.pdf  --pdf-engine=pdflatex
 
     os.system(f'git reset')
